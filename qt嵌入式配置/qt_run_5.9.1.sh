38e3b1ad5b9a440a2459c4d70b632a3c4fa8fb74
jaronho <892134825@qq.com>
2017-08-08 21:35:56 +0800

    qt嵌入式配置

---
+++ qt嵌入式配置/qt_run_5.9.1.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+./configure \
+-prefix /opt/qt-5.9.1-arm \
+-verbose \
+-no-feature-cursor \
+-opensource \
+-confirm-license \
+-release \
+-shared \
+-device linux-imx6-g++ \
+-device-option CROSS_COMPILE=/opt/poky/1.7/sysroots/x86_64-pokysdk-linux/usr/bin/arm-poky-linux-gnueabi/arm-poky-linux-gnueabi- \
+-sysroot /opt/poky/1.7/sysroots/cortexa9hf-vfp-neon-poky-linux-gnueabi \
+-I /opt/tslib-1.11/include \
+-L /opt/tslib-1.11/lib \
+-make libs \
+-make examples \
+-nomake tests \
+-qt-zlib \
+-no-opengl \
+-xcb-xlib \
+-eglfs \
+-linuxfb \
+-qt-xcb \
+-evdev \
+-libinput \
+-tslib \
+-gif \
+-ico \
+-qt-libpng \
+-qt-libjpeg \
+-qt-sqlite
