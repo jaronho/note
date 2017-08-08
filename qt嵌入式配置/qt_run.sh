2550398410a02454d70cc36291397ae011c55e44
jaronho <892134825@qq.com>
2017-08-08 11:50:29 +0800

    qt嵌入式配置

--- qt嵌入式配置/qt_run.sh
+++ qt嵌入式配置/qt_run.sh
@@ -4,7 +4,8 @@
 -opensource \
 -confirm-license \
 -release \
- -shared \
+-shared \
+-verbose \
 -embedded arm \
 -force-pkg-config \
 -xplatform qws/linux-arm-gnueabi-g++ \
@@ -17,31 +18,45 @@
 -qt-zlib \
 -qt-libpng \
 -qt-freetype \
--little-endian -host-little-endian \
+-little-endian \
+-host-little-endian \
 -no-qt3support \
--qt-libtiff -qt-libmng \
+-qt-libtiff \
+-qt-libmng \
 -make translations \
--qt-gfx-linuxfb -qt-gfx-transformed -qt-gfx-multiscreen \
--no-gfx-vnc -no-gfx-qvfb -qt-kbd-linuxinput \
--no-kbd-qvfb -armfpa  \
+-qt-gfx-linuxfb \
+-qt-gfx-transformed \
+-qt-gfx-multiscreen \
+-no-gfx-vnc \
+-no-gfx-qvfb \
+-qt-kbd-linuxinput \
+-no-kbd-qvfb \
+-armfpa \
 -no-mouse-qvfb \
 -no-opengl \
--no-mmx -no-sse -no-sse2 \
+-no-mmx \
+-no-sse \
+-no-sse2 \
 -no-3dnow \
 -no-openssl \
 -webkit \
 -no-qvfb \
 -no-phonon \
 -no-nis \
--no-opengl \
 -no-cups \
 -no-glib \
--no-xcursor -no-xfixes -no-xrandr -no-xrender \
+-no-xcursor \
+-no-xfixes \
+-no-xrandr \
+-no-xrender \
 -no-separate-debug-info \
--nomake examples -make tools -nomake docs \
--qt-mouse-tslib -DQT_QLOCALE_USES_FCVT \
--I/opt/tslib-1.11/include \
--L/opt/tslib-1.11/lib \
+-nomake docs \
+-nomake examples \
+-make tools \
+-qt-mouse-tslib \
+-DQT_QLOCALE_USES_FCVT \
+-I /opt/tslib-1.11/include \
+-L /opt/tslib-1.11/lib \
 -importdir /opt/qt-4.8.5-arm/qml 
 
 
