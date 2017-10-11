0f5d27a2ec6eafac94a761d73823fe73e9f878ba
jaronho <892134825@qq.com>
2017-10-11 11:23:32 +0800

    qt源码编译配置

--- qt源码编译配置/qt_run_5.9.1_desktop.sh
+++ qt源码编译配置/qt_run_5.9.1_desktop.sh
@@ -5,4 +5,12 @@
 -release \
 -shared \
 -confirm-license \
+-make libs \
+-nomake tools \
+-nomake examples \
+-nomake tests \
+-no-feature-cursor \
+-no-qml-debug \
+-no-use-gold-linker \
+-no-iconv \
 -prefix /opt/qt-5.9.1-desktop
